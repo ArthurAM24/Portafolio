59a92a851554008bdc6f8b7ab28373de5463b7c8
Arthur <arthurmoralesd24@gmail.com>
2023-10-23 10:55:34 -0500

    fix: comprimiendo img

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { HashRouter as Router } from "react-router-dom";
 import { About, Contact, Experience, Skills, Hero, Navbar, StarsCanvas, Proyects } from "./components";
+import { useEffect } from "react";
 
 const App = () => {
+  // Función de controlador de desplazamiento
+  const myScrollHandler = () => {
+      // Hacer que un elemento sea fijo o pegajoso
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', myScrollHandler, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', myScrollHandler);
+    };
+  }, []);
+
   return (
     <Router>
       <div className='relative z-0 bg-primary'>
