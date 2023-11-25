5549bcb49559471570968b807f929eeb9f822b6e
Arthur <arthurmoralesd24@gmail.com>
2023-11-24 22:37:56 -0500

    fix: Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='absolute xs:bottom-10 bottom-12 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-12 bottom-12 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
